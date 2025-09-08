caf61f837e76fd1b92ef43cf71f1db2ec7c3f362
Colin Jamison <colin.jamison@liatrio.com>
2025-09-08 11:12:43 -0700

    fix: handle division by zero
    Fixes #61

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ int main()
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
     std::cout << "Division: " << x / y << std::endl;
+    if (y==0) {
+        std::cout << "Dividing by zero is not a number.";
+    } else {
+        std::cout << "Division: " << x / y;
+    }
+    std::cout << std::endl;
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
